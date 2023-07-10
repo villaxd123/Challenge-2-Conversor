9cebdfd795e19d0519d35b8693aa165ec3fdd5e0
Jorge Villafuerte <jorge_123_386@hotmail.es>
2023-07-10 10:33:15 -0400

    Interface implemented on ConversorMonedas class

---
+++ src/ValorCambioMoneda.java
@@ -0,0 +1,24 @@
+public interface ValorCambioMoneda {
+    double[] valorDeCambio = {
+            0.14568375,
+            0.1326892,
+            0.11383094,
+            20.644433,
+            190.12289,
+            6.8579975,
+            7.5289907,
+            8.7820297,
+            0.048421795,
+            0.0052568927
+    };
+    String[] monedaReferencia = {
+            "$",
+            "€",
+            "£",
+            "¥",
+            "₩",
+            "Bs", "Bs", "Bs", "Bs", "Bs"
+    };
+
+    String convertir(String monto, int option);
+}
