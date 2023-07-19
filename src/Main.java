0dcdfc77cdb2b80beef653d85eb39e081ca5eb44
Jorge Villafuerte <jorge_123_386@hotmail.es>
2023-07-19 07:44:13 -0400

    Restructured the Main class.
    Signed-off-by: Jorge Villafuerte <jorge_123_386@hotmail.es>

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,7 @@
-import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-        String[] options = {"Conversor de Monedas", "Conversor de Temperatura", "Conversor de Unidades"};
-
-        Object optionSelected = JOptionPane.showInputDialog(
-                null,
-                "Escoja una opción",
-                "Conversor",
-                JOptionPane.INFORMATION_MESSAGE,
-                null,
-                options,
-                options[0]
-        );
-        if (optionSelected == "Conversor de Monedas") {
-            ConversorMonedas conversorMonedas = new ConversorMonedas();
-        } else if (optionSelected == "Conversor de Temperatura") {
-            System.out.println(2);
-        } else if (optionSelected == "Conversor de Unidades") {
-            System.out.println(3);
-        }
+        MainMenu mainMenu = new MainMenu();
+        mainMenu.openOptionSelected();
     }
 }
